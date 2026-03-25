60939809c8ba21d3fc11e3782d19ebfc324dc378
Nishwan-Kumar <nishwan291205@gmail.com>
2026-03-25 16:54:25 +0530

    Resolved the blog fetching error

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import Header from "@/Components/Header";
 import { Subscribe } from "@/Components/Subscribe";
 
 async function getBlogs() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+  const baseUrl = process.env.NEXTAUTH_URL || 'http://localhost:3000';
   
   try {
     const res = await fetch(`${baseUrl}/api/blog?public=true`, { cache: 'no-store' });
